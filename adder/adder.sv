9844b039a2b4c3dd4981f1fbe4d51b23403bc5e5
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-09-25 13:12:40 -0500

    Update adder.sv

--- adder/adder.sv
+++ adder/adder.sv
@@ -21,27 +21,12 @@ module ripple_adder_9 (input logic [8:0] XA, xsw,
 
 	fa fa0 (.a(XA[0]), .xsw(xsw[0]), .c(fn), .s(s[0]), .c_out(c1));
 	fa fa1 (.a(XA[1]), .xsw(xsw[1]), .c(c1), .s(s[1]), .c_out(c2));
-	fa fa2 (.XA(a[2]), .xsw(xsw[2]), .c(c2), .s(s[2]), .c_out(c3));
-	fa fa3 (.XA(a[3]), .xsw(xsw[3]), .c(c3), .s(s[3]), .c_out(c4));
-	fa fa4 (.XA(a[4]), .xsw(xsw[4]), .c(c4), .s(s[4]), .c_out(c5));
-	fa fa5 (.XA(a[5]), .xsw(xsw[5]), .c(c5), .s(s[5]), .c_out(c6));
-	fa fa6 (.XA(a[6]), .xsw(xsw[6]), .c(c6), .s(s[6]), .c_out(c7));
-	fa fa7 (.XA(a[7]), .xsw(xsw[7]), .c(c7), .s(s[7]), .c_out(c8));
-	fa fa8 (.XA(a[8]), .xsw(xsw[8]), .c(c8), .s(s[8]), .c_out(c_out));
+	fa fa2 (.a(XA[2]), .xsw(xsw[2]), .c(c2), .s(s[2]), .c_out(c3));
+	fa fa3 (.a(XA[3]), .xsw(xsw[3]), .c(c3), .s(s[3]), .c_out(c4));
+	fa fa4 (.a(XA[4]), .xsw(xsw[4]), .c(c4), .s(s[4]), .c_out(c5));
+	fa fa5 (.a(XA[5]), .xsw(xsw[5]), .c(c5), .s(s[5]), .c_out(c6));
+	fa fa6 (.a(XA[6]), .xsw(xsw[6]), .c(c6), .s(s[6]), .c_out(c7));
+	fa fa7 (.a(XA[7]), .xsw(xsw[7]), .c(c7), .s(s[7]), .c_out(c8));
+	fa fa8 (.a(XA[8]), .xsw(xsw[8]), .c(c8), .s(s[8]), .c_out(c_out));
     
 endmodule
-///////////////////////////////////////////////////////
-// module ripple_adder (
-// 	input  logic  [8:0] a, xsw,
-// 	input  logic  fn,
-	
-// 	output logic  [8:0] s,
-// 	output logic  cout
-// );
-             
-//         logic c3, c6; 
-//         ripple_adder_3 ra3_0 (.a(a[2:0]),   .xsw(xsw[2:0]),   .c_in(fn),  .s(s[2:0]),   .c_out(c3) );
-//         ripple_adder_3 ra3_1 (.a(a[5:3]),   .xsw(xsw[5:3]),   .c_in(c3),  .s(s[5:3]),   .c_out(c6) );
-//         ripple_adder_3 ra3_2 (.a(a[8:6]),   .xsw(xsw[8:6]),   .c_in(c6),  .s(s[8:6]),   .c_out(cout) );
-       
-// endmodule
