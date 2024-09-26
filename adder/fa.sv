d128e49af2cb811997a18721df4cf3bf704879a2
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-09-25 21:05:48 -0500

    Update fa.sv

--- adder/fa.sv
+++ adder/fa.sv
@@ -1,1 +1,12 @@
-
+module fa (input logic a, sw, c, fn //sw = switch data and xsw = sw data XORed with fn select
+           output logic s, c_out
+           );
+           logic xsw;
+    always_comb begin 
+	xsw = sw^fn;
+        s = a^xsw^c;
+        c_out = (a&xsw)|(xsw&c)|(a&c);
+        
+    end
+    
+endmodule
