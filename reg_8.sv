68c2873eff4a63ac9072468cf545d5a2cac3594a
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-09-25 21:02:22 -0500

    Update reg_8.sv

--- reg_8.sv
+++ reg_8.sv
@@ -10,7 +10,7 @@ module reg_8(input logic      Clk, Reset, Shift_In, Load, Shift_En,
         Data_Out <= 8'h0;
       else if (Load)
         Data_Out <= D;
-      else if (Shift_En)
+                else if (Shift)
         Data_Out <= {Shift_In, Data_Out[7:1]};
     end
     
