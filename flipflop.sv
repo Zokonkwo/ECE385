dd8d907d9efe283fe33d5132d83f74dc4522aa52
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-09-26 13:03:01 -0500

    Update flipflop.sv

--- flipflop.sv
+++ flipflop.sv
@@ -1,1 +1,21 @@
+moduole flipflop_x(input D, Clk, load, reset
+                 output Q);
+	logic Din;
+
+	always_ff @ (posedge clk)
+	begin
+		dout = Din;
+	end
+
+	always_comb begin
+		if(reset)
+			Din = 1'b0;
+		else if(load)
+			Din = D;
+		else
+			Din = Q;
+	end 
+
+endmodule
+
 
