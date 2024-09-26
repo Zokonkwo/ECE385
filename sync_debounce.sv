433cb57dc21608bd9ce7b79d3211b708d76a5ba5
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-09-26 16:43:04 -0500

    Update sync_debounce.sv

--- sync_debounce.sv
+++ sync_debounce.sv
@@ -1,4 +1,4 @@
-:)
+
 //Debouncer circuit inspired by https://forum.digikey.com/t/debounce-logic-circuit-vhdl/12573
 //Also serves as a synchronizer for pushbutton and switch (asynchronous) inputs
 //Notice that this circuit behaves differently under simulation as it does when synthesized
@@ -12,7 +12,7 @@ localparam COUNTER_WIDTH = 1;
 
 //synchronizer w/ debouncer (use for fpga buttons)
 module sync_debounce (
-	input  logic clk, 
+	input  logic Clk, 
 	input  logic d, 
 
 	output logic q
@@ -22,7 +22,7 @@ module sync_debounce (
 	logic [COUNTER_WIDTH : 0] counter;
 	
 
-	always_ff @(posedge clk) begin
+	always_ff @(posedge Clk) begin
 		ff1 <= d; // flop input once
 		ff2 <= ff1; // flop input twice
 
