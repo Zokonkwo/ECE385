af3f7fc9676a65a6350294bb324fefa7945407b9
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-09-25 19:44:34 -0500

    Update control.sv

--- control.sv
+++ control.sv
@@ -232,22 +232,3 @@ module reg_8(input logic      Clk, Reset, Shift_In, Load, Shift_En,
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-module reg_1(input logic      Clk, Reset, Shift_In, Load, Shift_En,
-            input logic       D,
-            output logic      Shift_Out,
-            output logic      Data_Out);
-
-
-    always_ff @ (posedge Clk)
-    begin
-      if(Reset)
-        Data_Out <= 8'h0;
-      else if (Load)
-        Data_Out <= D;
-      else if (Shift_En)
-        Data_Out <= {Shift_In, Data_Out[7:1]};
-    end
-    
-    assign Shift_Out = Data_Out[0];
-
-endmodule
