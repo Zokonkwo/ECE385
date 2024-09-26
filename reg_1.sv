1b2ebbf5e8162b074520b1d57a9bbc9926d3668b
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-09-25 20:47:24 -0500

    Update reg_1.sv

--- reg_1.sv
+++ reg_1.sv
@@ -7,11 +7,11 @@ module reg_1(input logic      Clk, Reset, Shift_In, Load, Shift_En,
     always_ff @ (posedge Clk)
     begin
       if(Reset)
-        Data_Out <= 8'h0;
+        Data_Out <= 1'h0;
       else if (Load)
         Data_Out <= D;
       else if (Shift_En)
-        Data_Out <= {Shift_In, Data_Out[7:1]};
+            Data_Out <= {Shift_In, Data_Out[0]};
     end
     
     assign Shift_Out = Data_Out[0];
