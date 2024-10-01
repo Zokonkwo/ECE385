438bbf1351c5b5d897be949f315189d9cab24b59
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-01 16:14:43 -0500

    Update 2:1_mux.sv

--- Lab5/2:1_mux.sv
+++ Lab5/2:1_mux.sv
@@ -1,13 +1,13 @@
 module mux_2_1 (input logic mio_en
                 input logic [15:0] bus_data, rdata
-                output logic [15:0] mdr_in
+                output logic [15:0] mux_out
 );
 
 always_comb
   begin
     if(mio_en == 1)
-        mdr_in = rdata;
+        mux_out = rdata;
     else
-        mdr_in = bus_data;
+        mux_out = bus_data;
   end
 endmodule
