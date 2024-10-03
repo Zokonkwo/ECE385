a777c7612551366d3637ecba004676897f78ad4a
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-03 16:55:05 -0500

    Update bit3_mux_2_1.sv

--- lab5.2/bit3_mux_2_1.sv
+++ lab5.2/bit3_mux_2_1.sv
@@ -1,1 +1,13 @@
-module bit3__mux_2_1()
+module bit3__mux_2_1(input logic select
+                     input logic [2:0] input1, input2,
+                     output logic [2:0] mux_2_1_out);
+
+  always_comb
+    begin
+      if(select == 1)
+        mux_2_1_out = input2;
+      else
+        mux_2_1_out = input1;
+    end
+  
+endmodule
