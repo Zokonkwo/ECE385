2f55b73b4b6993d95789b7acf51abc0dfedb9f7c
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-03 16:03:07 -0500

    Update reg_file.sv

--- lab5.2/reg_file.sv
+++ lab5.2/reg_file.sv
@@ -5,7 +5,79 @@ module reg_file(input logic [2:0] dr, sr1, sr2,
                 output logic [15:0] sr2_out,);
 
 
+// //general purpose registers
+load_reg #(.DATA_WIDTH(16)) gp1_reg (
+    .clk(clk),
+    .reset(reset),
 
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp2_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp3_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp4_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp5_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp6_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp7_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
+load_reg #(.DATA_WIDTH(16)) gp8_reg (
+    .clk(clk),
+    .reset(reset),
+
+    .load(),
+    .data_i(),
+
+    .data_q()
+);
 
 
   
