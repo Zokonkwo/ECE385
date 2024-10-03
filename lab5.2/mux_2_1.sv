443d06d6faf3d189c6c787a0380552d6c6e7256f
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-03 16:48:24 -0500

    Update mux_2_1.sv

--- lab5.2/mux_2_1.sv
+++ lab5.2/mux_2_1.sv
@@ -1,1 +1,12 @@
+module mux_2_1 (input logic select,
+                input logic [15:0] input1, input2,
+                output logic [15:0] mux_2_1_out);
 
+  always_comb
+    begin
+      if(select = 1)
+          mux_2_1_out = input2;
+      else
+          mux_2_1_out = input1;
+    end
+endmodule
