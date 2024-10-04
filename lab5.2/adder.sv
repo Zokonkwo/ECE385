6711b5cec7b892e0ea1a3b4b58cce723d1e1513e
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-04 14:20:39 -0500

    update

--- lab5.2/adder.sv
+++ lab5.2/adder.sv
@@ -1,3 +1,8 @@
 module adder (input logic [15:0] a, b
               output logic [15:0] adder_out);
+
+    always_comb
+        begin
+            adder_out = a + b;
+        end
 endmodule
--- lab5.2/alu.sv
+++ lab5.2/alu.sv
@@ -3,6 +3,17 @@ module alu (input logic [15:0] sr2mux, sr1out,
             output logic [15:0] alu_out);
 
 
-
+    always_comb
+    begin
+        if(aluk == 2'b00)
+            alu_out = sr2_mux + sr1_out;
+        else if(aluk == 2'b01)
+            alu_out = sr2_mux & sr1_out;
+        else if (aluk == 2'b10)
+            alu_out = sr2_mux;
+        else if (aluk == 2'b11)
+            alu_out =  ~(sr1_out);
+        
+    end
   
 endmodule
