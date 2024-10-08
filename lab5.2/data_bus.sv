c56557da2778a702bf47b85012a05990bab63919
JasonHart922 <113485657+JasonHart922@users.noreply.github.com>
2024-10-08 15:12:26 -0500

    Update data_bus.sv

--- lab5.2/data_bus.sv
+++ lab5.2/data_bus.sv
@@ -1,17 +1,17 @@
 module data_bus (input logic [15:0] gateMDR, gateALU, gatePC, gateMARMUX, default_x,
-                 input logic [3:0] data_select,
+                 input logic [3:0] databus_select,
                  output logic [15:0] databus_out);
     always_comb
         begin
-            if(data_select == 4'b0000)
+          if(databus_select == 4'b0000)
                 databus_out = default_x;
-            if(data_select == 4'b0001)
+          if(databus_select == 4'b0001)
                 databus_out = gateMDR;
-            else if(data_select == 4'b0010)
+          else if(databus_select == 4'b0010)
                 databus_out = gateALU;
-            else if(data_select == 4'b0100)
+          else if(databus_select == 4'b0100)
                 databus_out = gatePC;
-            else if(data_select == 4'b1000)
+          else if(databus_select == 4'b1000)
                 databus_out = gateMARMUX;
             else
                 databus_out = default_x;
