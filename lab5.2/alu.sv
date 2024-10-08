620b287f8caf02ee79642a18ba22e38e4ebc4b48
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-08 15:30:26 -0500

    Update alu.sv

--- lab5.2/alu.sv
+++ lab5.2/alu.sv
@@ -5,15 +5,14 @@ module alu (input logic [15:0] sr2mux, sr1out,
 
     always_comb
         begin
-        if(aluk == 2'b00)
+          if(aluk == 2'b00) //ADD
             alu_out = sr2_mux + sr1_out;
-        else if(aluk == 2'b01)
+          else if(aluk == 2'b01) //AND
             alu_out = sr2_mux & sr1_out;
-        else if (aluk == 2'b10)
+          else if (aluk == 2'b10) //PASSA
             alu_out = sr2_mux;
-        else if (aluk == 2'b11)
+          else if (aluk == 2'b11) //NOT
             alu_out =  ~(sr1_out);
-        
         end
     end
   
