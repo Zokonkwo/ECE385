ef4833f63c91a4a8ce1a55d6b43c922727138cb0
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-08 17:59:05 -0500

    Update reg_file2.sv

--- lab5.2/reg_file2.sv
+++ lab5.2/reg_file2.sv
@@ -1,2 +1,10 @@
-module reg_file2()
+module reg_file2(input logic clk,
+                input logic [2:0] dr, sr1, sr2,
+                input logic [15:0] bus_data,
+                input logic ld_reg,
+                output logic [15:0] sr2_out,
+                output logic [15:0] sr1_out,);
+
+
+  
 endmodule
