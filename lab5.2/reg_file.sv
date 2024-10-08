e7259e2db877dd665d633e42313a60a932ac9f2e
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-08 17:56:53 -0500

    Update reg_file.sv

--- lab5.2/reg_file.sv
+++ lab5.2/reg_file.sv
@@ -26,11 +26,46 @@ logic [15:0] gp1_out, gp2_out, gp3_out, gp4_out, gp5_out, gp6_out, gp7_out, gp8_
       else if(dr == 3'b111 && ld_reg == 1'b1)
         load8 = 1'b1;
     end
-
-
-
-
+   always_comb
+    begin
+      if(sr1 == 3'b000 && ld_reg == 1'b1)
+        sr1_out = gp1_out;
+      else if(sr1 == 3'b001 && ld_reg == 1'b1)
+        sr1_out = gp2_out;
+      else if(sr1 == 3'010 && ld_reg == 1'b1)
+        sr1_out = gp3_out;
+      else if(sr1 == 3'011 && ld_reg == 1'b1)
+        sr1_out = gp4_out;
+      else if(sr1 == 3'100 && ld_reg == 1'b1)
+        sr1_out = gp5_out;
+      else if(sr1 == 3'101 && ld_reg == 1'b1)
+        sr1_out = gp6_out;
+      else if(sr1 == 3'110 && ld_reg == 1'b1)
+        sr1_out = gp7_out;
+      else if(sr1 == 3'b111 && ld_reg == 1'b1)
+        sr1_out = gp8_out;
+    end
+   always_comb
+    begin
+      if(sr2 == 3'b000 && ld_reg == 1'b1)
+        sr2_out = gp1_out;
+      else if(sr2 == 3'b001 && ld_reg == 1'b1)
+        sr2_out = gp2_out;
+      else if(sr2 == 3'010 && ld_reg == 1'b1)
+        sr2_out = gp3_out;
+      else if(sr2 == 3'011 && ld_reg == 1'b1)
+        sr2_out = gp4_out;
+      else if(sr2 == 3'100 && ld_reg == 1'b1)
+        sr2_out = gp5_out;
+      else if(sr2 == 3'101 && ld_reg == 1'b1)
+        sr2_out = gp6_out;
+      else if(sr2 == 3'110 && ld_reg == 1'b1)
+        sr2_out = gp7_out;
+      else if(sr2 == 3'b111 && ld_reg == 1'b1)
+        sr2_out = gp8_out;
+    end
   
+
 // //general purpose registers
 load_reg #(.DATA_WIDTH(16)) gp1_reg (
     .clk(clk),
@@ -39,7 +74,7 @@ load_reg #(.DATA_WIDTH(16)) gp1_reg (
     .load(load1),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp1_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp2_reg (
     .clk(clk),
@@ -48,7 +83,7 @@ load_reg #(.DATA_WIDTH(16)) gp2_reg (
     .load(load2),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp2_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp3_reg (
     .clk(clk),
@@ -57,7 +92,7 @@ load_reg #(.DATA_WIDTH(16)) gp3_reg (
     .load(load3),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp3_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp4_reg (
     .clk(clk),
@@ -66,7 +101,7 @@ load_reg #(.DATA_WIDTH(16)) gp4_reg (
     .load(load4),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp4_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp5_reg (
     .clk(clk),
@@ -75,7 +110,7 @@ load_reg #(.DATA_WIDTH(16)) gp5_reg (
     .load(load5),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp5_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp6_reg (
     .clk(clk),
@@ -84,7 +119,7 @@ load_reg #(.DATA_WIDTH(16)) gp6_reg (
     .load(load6),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp6_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp7_reg (
     .clk(clk),
@@ -93,7 +128,7 @@ load_reg #(.DATA_WIDTH(16)) gp7_reg (
     .load(load7),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp7_out)
 );
 load_reg #(.DATA_WIDTH(16)) gp8_reg (
     .clk(clk),
@@ -102,7 +137,7 @@ load_reg #(.DATA_WIDTH(16)) gp8_reg (
     .load(load8),
     .data_i(bus_data),
 
-    .data_q()
+    .data_q(gp8_out)
 );
 
 
