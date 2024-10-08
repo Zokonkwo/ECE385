072176fddd0aa363a0b7aa7ddf7e03882c6318bd
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-08 15:59:45 -0500

    Update reg_file.sv

--- lab5.2/reg_file.sv
+++ lab5.2/reg_file.sv
@@ -2,7 +2,7 @@ module reg_file(input logic [2:0] dr, sr1, sr2,
                 input logic [15:0] bus_data,
                 input logic ld_reg,
                 output logic [15:0] sr2_out,
-                output logic [15:0] sr2_out,);
+                output logic [15:0] sr1_out,);
 
 
 // //general purpose registers
