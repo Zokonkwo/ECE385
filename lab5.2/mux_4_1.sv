806902a9d9d61ead9727e94e47e051d561a4f0de
Zikokonkwo <143134152+Zikokonkwo@users.noreply.github.com>
2024-10-09 18:07:07 -0500

    Update mux_4_1.sv

--- lab5.2/mux_4_1.sv
+++ lab5.2/mux_4_1.sv
@@ -1,15 +1,15 @@
-module mux_4_1 (input logic [2:0] adder2_select,
+module mux_4_1 (input logic [1:0] adder2_select,
                 input logic [15:0] sext1, sext2, sext3, zero_input,
                 output lofic [15:0] adder2_mux_out);
   always_comb
     begin
-      if(adder2_select == 3'b000)
+      if(adder2_select == 2'b00)
         adder2_mux_out = zero_input;
-      else if(adder2_select == 3'b001)
+      else if(adder2_select == 3'b01)
         adder2_mux_out = sext1;
-      else if(adder2_select == 3'b010)
+      else if(adder2_select == 3'b10)
         adder2_mux_out = sext2;
-      else if (adder2_select == 3'b011)
+      else if (adder2_select == 3'b11)
         adder2_mux_out = sext3;
     end
   end
